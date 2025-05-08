75a994dbb8bd2073df7b6dd5bf5962faefa6cecd
MartinaC <martiinacanteros@gmail.com>
2025-05-07 21:59:16 -0300

    .

--- server.ts
+++ server.ts
@@ -8,7 +8,7 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 
-const PORT = "5000";
+const PORT = process.env.PORT || "5000";
 const HOST = process.env.HOST || "localhost";
 const app= express();
 
